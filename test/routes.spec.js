f45a8587b851db5aa0649a22483222701fa77c6f
brickstar <brickstar@gmail.com>
2018-10-12 22:35:32 -0600

    Remove seed files for meals and meal_foods, Add seed data for foods sedfile, mocha

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -11,17 +11,35 @@ chai.use(chaiHttp);
 describe('API Routes', () => {
   before((done) => {
     database.migrate.latest()
-      .then(() => done())
-      .catch(error => {
-        throw error;
-      });
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
   });
 
   beforeEach((done) => {
     database.seed.run()
-      .then(() => done())
-      .catch(error => {
-        throw error;
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+
+  describe('GET /api/v1/foods', () => {
+    it('should return all of the foods', done => {
+      chai.request(server)
+      .get('/api/v1/foods')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].title.should.equal('apple');
+        response.body[0].should.have.property('calories');
+        response.body[0].author.should.equal(100);
+        done();
       });
+    });
   });
 });
