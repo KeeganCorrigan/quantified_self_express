2842654db8a04e2affeb0fb5d17a39340e52e1b0
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-17 13:35:07 -0600

    Add CORS

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
